9370f4fa37b0a3f352edef279d63637e9ed1e36f
Coders in Flow <codersinflow@localhost>
2025-08-27 20:29:14 -0400

    Auto-commit on startup - 2025-08-28T00:29:14.058Z

--- src/components.bak/InteractiveCardSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CardStacker from './CardStacker';
-
-// This wrapper component handles all the interactive card functionality
-const InteractiveCardSection: React.FC = () => {
-  return <CardStacker />;
-};
-
-export default InteractiveCardSection;
